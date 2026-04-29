4e681ae116113761b41f7f43b6158174b0236a5d
maelel <maelel@alumantflorida.es>
2026-04-29 12:03:14 +0200

    Cambio velocidad palas

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    public float speed = 14f;
+    public float speed = 10f;
     public Key upKey;
     public Key downKey;
 
